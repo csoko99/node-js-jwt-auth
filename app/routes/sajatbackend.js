6ee0a355d27771ed3046cd644d37c80dd939af02
csoko99 <laczkolali77@gmail.com>
2022-01-20 18:36:50 +0100

    dqw

--- app/routes/sajatbackend.js
+++ app/routes/sajatbackend.js
@@ -23,7 +23,7 @@ module.exports = function(app) {
     
     connection.connect()
     
-    connection.query('SELECT * from animek', function (err, rows, fields) {
+    connection.query('SELECT * from animek INNER JOIN leiras ON animek.anime_id=leiras.anime_leiras_id', function (err, rows, fields) {
        
       
       console.log(rows)
@@ -35,5 +35,244 @@ module.exports = function(app) {
  
 
   })
+  //--------------------------------------alap vége---------------------------------
+
+  //-------------------------------------szavazatfelvitel---------------------------
+
+  app.post('/szavazatfelvitel', (req, res) => {
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'animedb'
+    })
+    
+    connection.connect()
+    
+    connection.query('INSERT into szavazatok values (null, '+req.body.bevitel1+' )', function (err, rows, fields) {
+      if (err) throw err
+    
+      console.log("Szavazatát rögzítettük!")
+      res.send("Szavazatát rögzítettük!")
+    })
+    
+    connection.end()
+  
+  })
+
+  //-------------------------------------Megjelenés----------------------------------
+  app.get('/ketezer', (req, res) => {
+  
+
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'animedb'
+  })
+  
+  connection.connect()
+      
+      connection.query('SELECT * FROM `animek` WHERE `anime_megjdatum`<="2000-12-31"', function (err, rows, fields) {
+         // if (err) throw err;
+        
+        console.log(rows)
+        res.send(rows)
+      })
+      
+      connection.end()
+   
+   
+  
+    })
+  
+    app.get('/ezertiz', (req, res) => {
+    
+  
+      var mysql = require('mysql')
+      var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'animedb'
+    })
+    
+    connection.connect()
+        
+        connection.query('SELECT * FROM `animek` WHERE `anime_megjdatum`>"2000-12-31" AND anime_megjdatum<"2011-01-01"', function (err, rows, fields) {
+           // if (err) throw err;
+          
+          console.log(rows)
+          res.send(rows)
+        })
+        
+        connection.end()
+     
+     
+    
+      })
+  
+      app.get('/ezerhusz', (req, res) => {
+    
+  
+        var mysql = require('mysql')
+        var connection = mysql.createConnection({
+          host: 'localhost',
+          user: 'root',
+          password: '',
+          database: 'animedb'
+      })
+      
+      connection.connect()
+          
+          connection.query('SELECT * FROM `animek` WHERE `anime_megjdatum`>"2010-12-31" AND anime_megjdatum<"2021-01-01"', function (err, rows, fields) {
+             // if (err) throw err;
+            
+            console.log(rows)
+            res.send(rows)
+          })
+          
+          connection.end()
+       
+       
+      
+        })
+  
+        app.get('/huszfelett', (req, res) => {
+    
+  
+          var mysql = require('mysql')
+          var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'animedb'
+        })
+        
+        connection.connect()
+            
+            connection.query('SELECT * FROM `animek` WHERE `anime_megjdatum`>"2020-12-31"', function (err, rows, fields) {
+               // if (err) throw err;
+              
+              console.log(rows)
+              res.send(rows)
+            })
+            
+            connection.end()
+         
+         
+        
+          })
+          //-----------------------------------Műfaj szerinti keresés-----------
+
+          app.get('/Fantasy', (req, res) => {
+  
+
+            var mysql = require('mysql')
+            var connection = mysql.createConnection({
+              host: 'localhost',
+              user: 'root',
+              password: '',
+              database: 'animedb'
+          })
+          
+          connection.connect()
+              
+              connection.query('SELECT * FROM `animek` WHERE `anime_mufaj` LIKE "%Fantasy%"', function (err, rows, fields) {
+                 // if (err) throw err;
+                
+                console.log(rows)
+                res.send(rows)
+              })
+              
+              connection.end()
+           
+           
+          
+            })
+          
+          
+          app.get('/Drama', (req, res) => {
+            
+          
+            var mysql = require('mysql')
+            var connection = mysql.createConnection({
+              host: 'localhost',
+              user: 'root',
+              password: '',
+              database: 'animedb'
+          })
+          
+          connection.connect()
+              
+              connection.query('SELECT * FROM `animek` WHERE `anime_mufaj` LIKE "%Drama%"', function (err, rows, fields) {
+                 // if (err) throw err;
+                
+                console.log(rows)
+                res.send(rows)
+              })
+              
+              connection.end()
+           
+           
+          
+            })
+          
+          
+          
+          app.get('/Comedy', (req, res) => {
+            
+          
+            var mysql = require('mysql')
+            var connection = mysql.createConnection({
+              host: 'localhost',
+              user: 'root',
+              password: '',
+              database: 'animedb'
+          })
+          
+          connection.connect()
+              
+              connection.query('SELECT * FROM `animek` WHERE `anime_mufaj` LIKE "%Comedy%"', function (err, rows, fields) {
+                 // if (err) throw err;
+                
+                console.log(rows)
+                res.send(rows)
+              })
+              
+              connection.end()
+           
+           
+          
+            })
+          
+          app.get('/Action', (req, res) => {
+            
+          
+            var mysql = require('mysql')
+            var connection = mysql.createConnection({
+              host: 'localhost',
+              user: 'root',
+              password: '',
+              database: 'animedb'
+          })
+          
+          connection.connect()
+              
+              connection.query('SELECT * FROM `animek` WHERE `anime_mufaj` LIKE "%Action%"', function (err, rows, fields) {
+                 // if (err) throw err;
+                
+                console.log(rows)
+                res.send(rows)
+              })
+              
+              connection.end()
+           
+           
+          
+            })
   
 };
